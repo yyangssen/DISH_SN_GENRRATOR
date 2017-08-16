0544e780a4512bbeef31eee788dacb6978b39bd5
yyangssen <775651386@qq.com>
2017-08-16 15:39:24 +0800

    1.add Previous Button,2.add getTableException

---
+++ src/Exception/InputException.java
@@ -0,0 +1,11 @@
+package Exception;
+
+public class InputException extends Exception{
+
+	public InputException() {
+		// TODO Auto-generated constructor stub
+	}
+	public InputException(String msg) {
+		super(msg);
+	}
+}
---
+++ src/Exception/getTableException.java
@@ -0,0 +1,10 @@
+package Exception;
+import java.lang.*;
+public class getTableException extends Exception{
+	public getTableException() {
+		// TODO Auto-generated constructor stub
+	}
+	public getTableException(String msg) {
+		super(msg);
+	}
+}
